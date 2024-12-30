7fdd2e5adf88645c9513c7ab35b3f8c2ba45d8ac
trycatch <trycatch98@gmail.com>
2024-12-30 15:26:15 +0900

    fix: TextField의 높이가 콘텐츠를 따라가도록 변경

--- core/designsystem/src/main/kotlin/com/trycatch/designsystem/component/AuroraTextField.kt
+++ core/designsystem/src/main/kotlin/com/trycatch/designsystem/component/AuroraTextField.kt
@@ -35,6 +35,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.Text
@@ -95,7 +96,7 @@ fun AuroraTextField(
                 Column(
                     modifier = Modifier
                         .weight(1f)
-                        .fillMaxHeight()
+                        .wrapContentHeight()
                 ) {
                     Text(
                         modifier =  Modifier
@@ -139,8 +140,8 @@ private fun AuroraTextFieldPreview() {
 private fun AuroraTextFieldValuePreview() {
     AuroraTheme {
         AuroraTextField(
-            modifier = Modifier.size(327.dp, 64.dp),
-            value = "then vacant hub ride avoid girl alien cross usage exist comic upon",
+            modifier = Modifier.wrapContentHeight(),
+            value = "then vacant hub ride avoid girl alien cross usage exist comic upon then vacant hub ride avoid girl alien cross usage exist comic upon",
             label = "Seed Phrase",
             onValueChange = {}
         )
